7f3a1fbe3da9bcd8ef22275cf6dda3cc64a7a426
Shruti029 <59735421+Shruti029@users.noreply.github.com>
2021-06-24 23:42:05 +0200

    Update test_subtraction.py

--- test/temp/test_subtraction.py
+++ test/temp/test_subtraction.py
@@ -4,4 +4,4 @@ def test_subtraction():
     assert subtraction.subtraction([1,1],[2,2])[0] == -1
     assert subtraction.subtraction([1,1],[2,2])[1] == -1
     assert subtraction.subtraction([1,1],[3,5])[0] == -2
-    assert subtraction.subtraction([1,1],[3,5])[0] == -4
+    assert subtraction.subtraction([1,1],[3,5])[1] == -4
