02c5340a0d6740c8475498de6991e39713e13097
Shruti029 <59735421+Shruti029@users.noreply.github.com>
2021-06-24 23:22:49 +0200

    Delete test_main.py

--- test/test_main.py
+++
@@ -1,9 +0,0 @@
-import main
-
-
-def test_startApp():
-    assert main.startApp() == "Welcome to Matrix calculator!"
-
-
-def test_endApp():
-    assert main.endApp == "Thank you for using Matrix calculator!"
