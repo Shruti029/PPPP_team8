b37083dfedaa15edc66c2926038d0cc939bc62fc
Shruti029 <59735421+Shruti029@users.noreply.github.com>
2021-06-24 23:13:49 +0200

    Update test_main.py

--- test/test_main.py
+++ test/test_main.py
@@ -2,6 +2,7 @@ import main
 
 def test_startApp():
     assert main.startApp() == "Welcome to Matrix calculator!"
-    
+
+
 def test_endApp():
     assert main.endApp == "Thank you for using Matrix calculator!"
