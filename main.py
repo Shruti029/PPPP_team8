992ebfd0ea6c209594babbd711aa515316620b7d
Shruti029 <59735421+Shruti029@users.noreply.github.com>
2021-06-24 20:25:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ Created on Wed Jun 23 20:36:53 2021
 from addition import addition
 from subtraction import subtraction
 
-print ("Welcome to Matrix calculator!")
+print("Welcome to Matrix calculator!")
 
 row1 = int(input("Enter number of numbers in arrays:"))
 
@@ -18,18 +18,18 @@ array2 = []
 
 for i in range(row1):
     array1.append(int(input("Enter element of Array 1:")))
-    
+
 for i in range(row1):
     array2.append(int(input("Enter element of Array 2:")))
     
     
 operation = str(input("Choose operation (+, -):"))
 
-if(operation=='+'):
-    print("Answer:",addition(array1,array2))
+if operation == '+':
+    print("Answer:", addition(array1,array2))
     
-if(operation=='-'):
-    print("Answer:",subtraction(array1,array2))
+if operation == '-':
+    print("Answer:", subtraction(array1,array2))
     
 
     
