d2216d132dbddaab899c1252a3fcb99102c0c2e7
Shruti029 <59735421+Shruti029@users.noreply.github.com>
2021-06-25 00:59:59 +0200

    Update test_addition.py

--- test/temp/test_addition.py
+++ test/temp/test_addition.py
@@ -2,7 +2,7 @@ import addition
 
 
 def test_addition():
-    assert addition.addition([1,1],[2,2])[0] == 3
-    assert addition.addition([1,1],[2,2])[1] == 3
-    assert addition.addition([1,1],[3,5])[0] == 4
-    assert addition.addition([1,1],[3,5])[1] == 6
+    assert addition.addition([1, 1], [2, 2])[0] == 3
+    assert addition.addition([1, 1], [2, 2])[1] == 3
+    assert addition.addition([1, 1], [3, 5])[0] == 4
+    assert addition.addition([1, 1], [3, 5])[1] == 6
