abd7bcac561e2beac1b8d631a46c760ebb3f583c
Shruti029 <59735421+Shruti029@users.noreply.github.com>
2021-06-25 01:10:23 +0200

    Adding new functionality (multiplication) - initial commit

---
+++ multiplication.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 24 22:25:54 2021
+
+@author: chawane
+"""
+
+import numpy as np
+
+def multiplication(arr1, arr2):
+    return (np.multiply(arr1,arr2))
+
+# print(multiplication([1,2],[2,3]))
