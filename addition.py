debe2ee43cfa902bb85aba6b00f4f25bb5ec8ed6
Shruti029 <59735421+Shruti029@users.noreply.github.com>
2021-06-24 20:20:35 +0200

    Update addition.py

--- addition.py
+++ addition.py
@@ -7,8 +7,9 @@ Created on Thu Jun 24 10:51:40 2021
 
 import numpy as np
 
-def addition(arr1, arr2):
-    return (np.add(arr1,arr2))
 
-#print(addition([1,2],[2,3]))
+def addition(arr1, arr2):
+    return np.add(arr1,arr2)
+
+# print(addition([1,2],[2,3]))
     
