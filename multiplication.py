d39f0a3a779bc7a1132499bbabe2393d521989d6
Shruti029 <59735421+Shruti029@users.noreply.github.com>
2021-06-25 01:24:13 +0200

    Update multiplication.py

--- multiplication.py
+++ multiplication.py
@@ -7,7 +7,8 @@ Created on Thu Jun 24 22:25:54 2021
 
 import numpy as np
 
+
 def multiplication(arr1, arr2):
-    return (np.multiply(arr1,arr2))
+    return (np.multiply(arr1, arr2))
 
 # print(multiplication([1,2],[2,3]))
